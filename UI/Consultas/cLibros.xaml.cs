0d921f7c38ce0a900fd161bc4ecf8030703ad542
pablouix <pablouix@gmail.com>
2022-02-01 21:57:03 -0400

    Creando Consulta y UI

---
+++ UI/Consultas/cLibros.xaml.cs
@@ -0,0 +1,23 @@
+using System.Windows;
+using System.Linq;
+using System.Collections.Generic;
+using Pablo_Burgos_Ap1_p1.BLL;
+
+namespace Pablo_Burgos_Ap1_p1.UI.Consultas
+{
+    public partial class cLibros : Window
+    {
+
+        public cLibros()
+        {
+            InitializeComponent();
+        }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+           
+
+        }
+
+    }
+}
