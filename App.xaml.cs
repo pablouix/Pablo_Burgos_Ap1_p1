21211db55891062a0b6803b8ed11b52c4266e4ef
pablouix <pablouix@gmail.com>
2022-02-01 21:23:10 -0400

    Creacion de carpetas y entity Framework

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Pablo_Burgos_Ap1_p1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
