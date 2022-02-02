d5d85748886710bf7c09b98cb4a09d213ed52410
pablouix <pablouix@gmail.com>
2022-02-01 21:53:17 -0400

    Creando Registro y UI

---
+++ UI/Registros/rLibros.xaml.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Windows;
+using System.Linq;
+using System.Collections.Generic;
+using Pablo_Burgos_Ap1_p1.BLL;
+
+namespace Pablo_Burgos_Ap1_p1.UI.Registros
+{
+    public partial class rLibros : Window
+    {
+        public rLibros()
+        {
+            InitializeComponent();
+            // this.DataContext = Libro;
+        }
+
+        private void Cargar()
+        {
+
+        }
+
+        private void Limpiar()
+        {
+        }
+
+        private bool Validar()
+        {
+            bool esValido = true;
+
+
+
+            return esValido;
+        }
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void NuevoButton_Click(object sender, RoutedEventArgs e)
+        {
+             
+        }
+
+        private void GuardarButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void EliminarButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
