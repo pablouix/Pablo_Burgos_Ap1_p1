bfca25381b0f0e96459716ce5e45e3ad3874f689
pablouix <pablouix@gmail.com>
2022-02-01 21:32:35 -0400

    Agregando BLL

---
+++ BLL/BLL.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Pablo_Burgos_Ap1_p1.BLL
+{
+    public class BLL
+    {
+        
+    }
+}
