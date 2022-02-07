3a4b290e01a64bc21e3614a5ca5f1198205e4d59
pablouix <pablouix@gmail.com>
2022-02-07 18:11:30 -0400

    Entidad

---
+++ Entidades/Productos.cs
@@ -0,0 +1,15 @@
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Pablo_Burgos_Ap1_p1.Entidades
+{
+    public class Productos
+    {
+        [Key]
+        public int ProductosId { get; set; }
+        public string Descripcion { get; set; }
+        public string Existencia {get; set; }
+        public float Costo { get; set; }
+        public float ValorInventario { get; set; }
+    }
+}
