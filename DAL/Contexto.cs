7c0027dd7b76fa5145c807084747561b6e3a8494
pablouix <pablouix@gmail.com>
2022-02-07 19:00:22 -0400

    contexto publico

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -3,7 +3,7 @@ using Pablo_Burgos_Ap1_p1.Entidades;
 
 public class Contexto:DbContext
 {
-    DbSet<Productos>? Productos {get; set; }
+    public DbSet<Productos>? Productos {get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlite(@"Data Source=DATA\Productos.db");
