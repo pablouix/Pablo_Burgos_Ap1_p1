64428a0ef476ef61ab37c364aeeeda75211e41e7
pablouix <pablouix@gmail.com>
2022-02-07 19:21:00 -0400

    Agregando migracion

---
+++ Migrations/20220207231910_migracion_Inicial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pablo_Burgos_Ap1_p1.Migrations
+{
+    public partial class migracion_Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    ProductosId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: false),
+                    Existencia = table.Column<string>(type: "TEXT", nullable: false),
+                    Costo = table.Column<float>(type: "REAL", nullable: false),
+                    ValorInventario = table.Column<float>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.ProductosId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Productos");
+        }
+    }
+}
