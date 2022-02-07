ddf535e8d4a1ba29741f188cfe6db6823a998dcb
pablouix <pablouix@gmail.com>
2022-02-07 18:20:10 -0400

    Contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,10 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+using Pablo_Burgos_Ap1_p1.Entidades;
 
 public class Contexto:DbContext
 {
+    DbSet<Productos>? Productos {get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlite(@"Data Source=DATA\Libros.db");
+        optionsBuilder.UseSqlite(@"Data Source=DATA\Productos.db");
 
     }
 }
